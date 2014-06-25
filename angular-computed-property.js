75c522b87d7416ffb14b5579275314a6d6404627
Ian Lovett <i.c.lovett@gmail.com>
2014-06-25 10:19:11 -0700

    removed extension of $rootScope

--- angular-computed-property.js
+++ angular-computed-property.js
@@ -10,8 +10,6 @@
    *
    * Factory which provides computed properties.
    *
-   * Extends $rootScope with $property function
-   *
    * Code from http://stackoverflow.com/questions/11216651/computed-properties-in-angular-js
    */
   angular.module('ngComputedProperty', [])
@@ -30,12 +28,6 @@
         updateProperty(scope);
 
       };
-    })
-    .run(function($rootScope, property) {
-      // add $property function to $scope
-      $rootScope.$property = function(propertyName, watchProperties, fn) {
-        property(this, propertyName, watchProperties, fn);
-      }
     });
 
 })(window, window.angular);
